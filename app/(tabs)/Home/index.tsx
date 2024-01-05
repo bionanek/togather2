4366c37bd6600724d62f90332ab088213ef51157
Jakub Urban <jakub.urban@autozone.com>
2024-01-05 17:27:20 +0100

    added userId page for testing route parameters

--- app/(tabs)/Home/index.tsx
+++ app/(tabs)/Home/index.tsx
@@ -1,11 +1,24 @@
-import { Stack } from "expo-router";
+import { Link, Stack } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Home() {
   return (
     <View>
       <Stack.Screen options={{ headerShown: true, title: "Home" }} />
-      <Text>Fookin Home page innit :)</Text>
+      <View style={{ marginBottom: 20 }}>
+        <Text>Fookin Home page innit :)</Text>
+      </View>
+      <Text>You wanna test navigating with route parameters? Aight!</Text>
+      <Text>
+        Pressie pressie them links below. Come on now mate! It's bloody fun,
+        innit!{" "}
+      </Text>
+      <Link href="/Profile/bobOne" style={{ fontWeight: "600" }}>
+        Go to user 'bobOne'
+      </Link>
+      <Link href="/Profile/bobTwo" style={{ fontWeight: "600" }}>
+        Go to user 'bobTwo'
+      </Link>
     </View>
   );
 }
