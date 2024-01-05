22cef67c54d2c9b8b82223e629f08f7fd22fc09c
aman.arnold <aman.arnold@yahoo.com>
2024-01-05 15:45:02 +0000

    add react native papers and search bar

--- app/(tabs)/Home/index.tsx
+++ app/(tabs)/Home/index.tsx
@@ -1,10 +1,20 @@
+import React from "react";
 import { Text } from "react-native";
+import { Searchbar } from "react-native-paper";
 
 export default function Home() {
-  return <Text>Fookin Home page innit</Text>;
+  const [searchQuery, setSearchQuery] = React.useState("");
+
+  const onChangeSearch = (query) => setSearchQuery(query);
+
+  return (
+    <>
+      <Text>Fookin Home page innit</Text>
+      <Searchbar
+        placeholder="Search"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+      />
+    </>
+  );
 }
-
-// app -> Home -> index.tsx
-// app -> home -> Discover.tsx
-
-// /home/home
