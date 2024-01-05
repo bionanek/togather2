f5a71a6e8312c6748d179e0128a635cdf84dc4af
aman.arnold <aman.arnold@yahoo.com>
2024-01-05 16:49:07 +0000

    add cspell and further add home screen components

--- app/(tabs)/Home/index.tsx
+++ app/(tabs)/Home/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Text } from "react-native";
-import { Searchbar } from "react-native-paper";
+import { IconButton, Searchbar } from "react-native-paper";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Home() {
   const [searchQuery, setSearchQuery] = React.useState("");
@@ -8,13 +8,45 @@ export default function Home() {
   const onChangeSearch = (query) => setSearchQuery(query);
 
   return (
-    <>
-      <Text>Fookin Home page innit</Text>
+    <View style={styles.page}>
+      <View style={styles.userContainer}>
+        <View style={styles.smallerUserContainer}>
+          <Text style={styles.userGreeting}>Hello</Text>
+          <Text style={styles.username}>Mr lil Shit</Text>
+        </View>
+        <IconButton
+          icon="camera"
+          size={20}
+          style={styles.userIcon}
+          onPress={() => console.log("Pressed")}
+        ></IconButton>
+      </View>
       <Searchbar
         placeholder="Search"
         onChangeText={onChangeSearch}
         value={searchQuery}
       />
-    </>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  page: {
+    margin: 20,
+    flexDirection: "column",
+  },
+  userContainer: {
+    flexDirection: "row",
+  },
+  smallerUserContainer: {},
+  userGreeting: {
+    color: "#ff8d02",
+  },
+  username: {
+    color: "#000000",
+  },
+  userIcon: {
+    alignItems: "flex-end",
+    justifyContent: "flex-end",
+  },
+});
