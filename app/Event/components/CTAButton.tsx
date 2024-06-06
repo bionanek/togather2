497ff36f1b4ca7749f2470ffc70af1fc445e67b1
Jakub Urban <jakub.urban@autozone.com>
2024-06-07 00:43:12 +0200

    feat: add participants avatars and right arrow circle icon to 'check out other participants' button in event page.

--- app/Event/components/CTAButton.tsx
+++ app/Event/components/CTAButton.tsx
@@ -1,30 +1,39 @@
 import { Colors2G } from '@constants/Colors'
 import React from 'react'
-import { StyleSheet, Text, TouchableHighlight } from 'react-native'
+import { StyleSheet, Text, TouchableHighlight, View, Image, FlexStyle } from 'react-native'
+import RightArrowIcon from '@icons/OtherIcons/right-arrow-icon.svg'
 
 type ButtonConfig = {
 	bgColor: string
 	textColor: string
 	callToAction: string
+  joinersAvatars?: string[]
+  justifyContent: FlexStyle['justifyContent']
+
 }
 
 type Props = {
 	onPress: () => void
 	type: 'join' | 'check-participants'
+  avatars?: string[]
 }
 
-const CTAButton: React.FC<Props> = ({ onPress, type }: Props) => {
+const CTAButton: React.FC<Props> = ({ onPress, type, avatars }: Props) => {
 	const buttonConfig: ButtonConfig =
 		type === 'join'
 			? {
 					bgColor: Colors2G.Accent,
 					textColor: 'white',
-					callToAction: 'I want to join this event'
+					callToAction: 'I want to join this event',
+          justifyContent: 'center'
 				}
 			: {
 					bgColor: Colors2G.AccentBackground,
 					textColor: 'black',
-					callToAction: 'Check out other participants'
+					callToAction: 'Check out other participants',
+          joinersAvatars: avatars,
+          justifyContent: 'flex-start'
+
 				}
 
 	return (
@@ -32,30 +41,79 @@ const CTAButton: React.FC<Props> = ({ onPress, type }: Props) => {
 			style={[
 				styles.joinButtonContainer,
 				{
-					backgroundColor: buttonConfig.bgColor
+					backgroundColor: buttonConfig.bgColor,
 				}
 			]}
 			onPress={onPress}
 			underlayColor={Colors2G.DarkerAccent}
 		>
-			<Text style={[styles.joinButtonText, { color: buttonConfig.textColor }]}>{buttonConfig.callToAction}</Text>
+      <View style={[styles.ctaContainer, {justifyContent: buttonConfig.justifyContent}]}>
+        {type === 'check-participants' ? <UsersAvatars avatars={avatars} /> : null}
+        <Text style={[styles.joinButtonText, { color: buttonConfig.textColor }]}>{buttonConfig.callToAction}</Text>
+        {type === 'check-participants' ?  (
+          <View style={styles.iconContainer}>
+            <RightArrowCircle />
+          </View>
+        ) : null}
+      </View>
 		</TouchableHighlight>
 	)
 }
 
 export default CTAButton
 
+
+const UsersAvatars = ({ avatars }: { avatars?: string[] }) => {
+  return (
+  <View style={styles.avatarsRow}>
+    { avatars?.map((avatar, index) => 
+      <Image
+        key={index}
+				style={[styles.avatar, { marginLeft: index === 0 ? 0 : -15 }]}
+				source={{
+					uri: avatar
+				}}
+    />)}
+  </View>)
+}
+
+const RightArrowCircle = () => (
+  <View style={styles.rightArrowCircle}>
+    <RightArrowIcon color={'white'}  width={11} height={11} />
+  </View>
+)
+
 const styles = StyleSheet.create({
+  iconContainer: { alignItems: 'flex-end', flex: 1},
+  rightArrowCircle: { borderRadius: 50, backgroundColor: Colors2G.Accent, padding: 7},
+  ctaContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  avatarsRow: {
+    flexDirection: 'row',
+    
+    marginRight: 10
+  },
+  avatar: {
+		width: 30,
+		height: 30,
+		borderRadius: 20,
+    borderWidth: 2,
+    borderColor: 'white'
+	},
 	joinButtonContainer: {
 		alignSelf: 'center',
 		padding: 20,
+    paddingHorizontal: 10,
 		width: '100%',
 		marginTop: 30,
 		borderRadius: 12
 	},
 	joinButtonText: {
 		color: 'white',
+    fontSize: 12,
 		fontWeight: '600',
-		textAlign: 'center'
+		textAlign: 'center',
 	}
 })
--- app/Event/index.tsx
+++ app/Event/index.tsx
@@ -37,6 +37,14 @@ const userData: User = {
 	rating: 4.5
 }
 
+
+const usersAvatarsMock = [
+  'https://s3-alpha-sig.figma.com/img/30b9/40a6/27dca9d0eb5704ffc2fb2d5c8aa7909e?Expires=1717372800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=MOwz7oi1cItoGTEUngIRX~yldVqlXOXve5Bh7ASUphoyshnacVMqi6nKvHcFpflB9rWvr~rn5~F0BhBTSI4lYNFZip1f~YJvEqi0LzfLSkngy6C1ro2ChtW-7onnTdD00ge6Qvzg4IxVuwfQ0kZoTtGUqfDBFXIL4UcNU60z7bwD4ORMxlap0eCncI8XEeO1HIYsCrfT-bZmJn2UKca7ZZMmaFwQ7RG1aJjCybuISINAcZPvamDuvdAGwo0MjpNlCKaxV8c2yzm4ReoUVpxp3De4q9bYzjZjKGAw2xvQoz1OkpfO1VPB6W1I3Qbq5sb5CHKTU6fMFMCGZy6zAghqzA__',
+  'https://s3-alpha-sig.figma.com/img/9773/1781/9bd81f9740aeb94a2b5a33dcc578550e?Expires=1718582400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cwOTlhMbwDH8Ov18SfwbONSRbJp5SvulRcEiCbRlMuH8T6RIZCc8zx6vHXtA8vyUkiodQjNeBC5wRYAZTX1YPruRq8J9Z3QLZ3p3hoR88nDN8KeXB7lLSrYx1pgLQoGgwmbF5oQXH7v8FL9WKASFLc2dNsCaEmpExGfPuOb8Kg29hjxGBtUOA7IOTkQ9j4Emz~0Oh61yvt0bXm7m89oGyoMiI-QupHslE~PdIBmKIsAQZJB4VdhOyF7IzeHZcYhXtfsPiD891URh67xJTSUWYFejoXKxiR0OHc46CLsa-IxFxW-8AWjoWElwKQNzJDyOhcJf6cAKV8O0cE~M1CXVTg__',
+  'https://s3-alpha-sig.figma.com/img/fdb2/67bd/f72c1e57edd07a38c95e9d0d9feb4c8d?Expires=1718582400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TKVRYJdSibjW1-T8CVZ92VZuwx4W-nMpTSScKl1gVzXR88AjRTeN3TXBypNTHmrJt5dCco4x0SvF1Wov8P7zzIDJG0Iahvh2O9IbxXq19TRg0LqU14LAg5ZGR1NpHFyKrl9~6PWdLpc53mnsVHzdJnVEBL6IOMdf~pM-9lg3AiQ1Dql-F5GbiHd1zqG-knBc3l0A-BfX~jhP7x5ZheQMp11KWJQCvJYb1mVJkapyYyAMpqO8MYecnkqvT4yFdJXoAjnKWZlu6XgaK0SeXq1CygXUZH1o2Wvmh-wa3UMv29VDBWQrMBCoGYctaVZMVAr7ml53IA2F5phcDFCRxbiJ1g__',
+  'https://s3-alpha-sig.figma.com/img/209f/54c9/d93369696573dd2d7a43d0048947eb01?Expires=1718582400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=NMIC4jspnpKNM2d-g5lwyNY4wIPTPMDohW-y2VImvt0ec9ZqVfXq7BFWu4CC2QjCdFOstEuCuM8xh94iwBcJo4jxba~NKwKBZkOBEo3dCz0w9lFUs1NiW1fDagFzhGIJ5TAjY7ZYC~c2yJeTmSFPpldxN-r-Ce7NB1V90xKb-eqzT7q0iyLAioOv3i9Mya5K~KBoHbWaFVO~4qTgktWeUTcNnUSGn-kfzqcdVMrC-c0J5uls9Uc8toGgQAV-LhYJlV6uIIT5WTV9fg49NgAq~5~wCweqo0JyRx~0F3Qhoj10KexvX525EfNP-4~sqsrwiHLDLQHksMjPI3wbZlWuqQ__'
+]
+
 export default function EventPage() {
 	const [isFavourite, setIsFavourite] = useState<boolean>(false)
 
@@ -84,6 +92,7 @@ export default function EventPage() {
 			/>
 			<CTAButton
 				onPress={onCheckoutParticipantsPress}
+        avatars={usersAvatarsMock}
 				type="check-participants"
 			/>
 
